9ef3eca62dbfce5b669d5b815a85129c61f95bc7
isaacMxS <isaac.mejia.est@tecazuay.edu.ec>
2023-06-08 20:47:40 -0500

    funcionamiento en iOS

--- lib/Services/globals.dart
+++ lib/Services/globals.dart
@@ -1,3 +1,4 @@
 //10.0.2.2
-const String baseUrl = "http://192.168.0.104:8080/api";
+//10.0.1.6(aisac)
+const String baseUrl = "http://10.0.1.6:8080/api";
 const Map<String, String> headers = {"Content-Type": "application/json"};
