4381a4b85995102a07228b182412716be97a32ba
thony239 <anthony.morocho.est@tecazuay.edu.ec>
2023-06-08 21:17:33 -0500

    ventanasssss :angel:

--- lib/Screens/lista_libros_screen.dart
+++ lib/Screens/lista_libros_screen.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:proyectoistateca/Services/database_services_libro.dart';
 import 'package:proyectoistateca/libro_tile.dart';
 import 'package:proyectoistateca/models/libros.dart';
-import 'package:proyectoistateca/models/tipos.dart';
 import 'package:proyectoistateca/models/tipos_data.dart';
 import 'package:proyectoistateca/widgets/widget_menu_lateral.dart';
 
@@ -14,8 +13,16 @@ class LlibrosScreen extends StatefulWidget {
   State<LlibrosScreen> createState() => _LlibrosScreenState();
 }
 
-class _LlibrosScreenState extends State<LlibrosScreen> {
+class _LlibrosScreenState extends State<LlibrosScreen> with SingleTickerProviderStateMixin {
   List<Libro>? libros;
+  late TabController _tabController;
+  List<Tab> _tabs = [
+    Tab(text: 'Todos'),
+    Tab(text: 'Aventura'),
+    Tab(text: 'Historia'),
+    Tab(text: 'Fantacia'),
+  ];
+  String _selectedEstado = 'Todos';
 
   getLibros() async {
     libros = await Database_services_libro.getLibro();
@@ -32,6 +39,7 @@ class _LlibrosScreenState extends State<LlibrosScreen> {
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: _tabs.length, vsync: this);
     getLibros();
   }
 
@@ -53,58 +61,80 @@ class _LlibrosScreenState extends State<LlibrosScreen> {
                 ),
               ),
               backgroundColor: Color.fromRGBO(24, 98, 173, 1.0),
-            ),
-            body: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              child: Consumer<TiposData>(
-                builder: (context, tiposData, child) {
-                  return Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        child: Text(
-                          'Mantenga presionado para obtener una vista previa del libro',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                      TextField(
-                        decoration: const InputDecoration(
-                          hintText: "Buscar libro",
-                          prefixIcon: Icon(Icons.search),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(25.0),
-                            ),
-                          ),
-                        ),
-                        onChanged: (value) {
-                          if (value.isNotEmpty) {
-                            getLibrosnombre(value);
-                          } else {
-                            getLibros();
-                          }
-                        },
-                      ),
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: tiposData.libros.length,
-                          itemBuilder: (context, index) {
-                            Libro libro = tiposData.libros[index];
-                            return LibrosTile(
-                              libro: libro,
-                              tiposData: tiposData,
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  );
-                },
+              bottom: TabBar(
+                controller: _tabController,
+                tabs: _tabs,
               ),
             ),
+            body: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    'Mantenga presionado para obtener una vista previa del libro',
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ),
+                TextField(
+                  decoration: const InputDecoration(
+                    hintText: "Buscar libro",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25.0),
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {
+                    if (value.isNotEmpty) {
+                      getLibrosnombre(value);
+                    } else {
+                      getLibros();
+                    }
+                  },
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: Provider.of<TiposData>(context).libros.length,
+                    itemBuilder: (context, index) {
+                      Libro libro = Provider.of<TiposData>(context).libros[index];
+                      return LibrosTile(
+                        libro: libro,
+                        tiposData: Provider.of<TiposData>(context),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            bottomNavigationBar: TabBarView(
+              controller: _tabController,
+              children: [
+                _buildTodosScreen(),
+                _buildPendientesScreen(),
+                _buildAprobadosScreen(),
+                _buildRechazadosScreen(),
+              ],
+            ),
           );
+  }
+
+  Widget _buildTodosScreen() {
+    return Container();
+  }
+
+  Widget _buildPendientesScreen() {
+    return Container();
+  }
+
+  Widget _buildAprobadosScreen() {
+    return Container();
+  }
+
+  Widget _buildRechazadosScreen() {
+    return Container();
   }
 }
