0eab75d5eb9f7727f32377554ff6b86a51220a82
isaacMxS <isaac.mejia.est@tecazuay.edu.ec>
2023-06-15 00:59:53 -0500

    correccion Login iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,15 +1,12 @@
 import UIKit
 import Flutter
-import GoogleSignIn
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate, GIDSignInDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GIDSignIn.sharedInstance().clientID = "228677746372-9bsir5j41ojaurgn28ok1hrurb25ic7m"
-    GIDSignIn.sharedInstance().delegate = self
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
