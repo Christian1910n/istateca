410c293152ea645345026bbd6655e21c52c82b8c
isaacMxS <isaac.mejia.est@tecazuay.edu.ec>
2023-07-24 18:36:10 -0500

    intento lista de carreras

--- lib/Screens/sugerencias_screen.dart
+++ lib/Screens/sugerencias_screen.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:proyectoistateca/Services/globals.dart';
+import 'package:proyectoistateca/models/carrera.dart';
+import 'package:proyectoistateca/models/sugerencia.dart';
 import 'package:http/http.dart' as http;
 
 class SugerenciasScreen extends StatefulWidget {
@@ -14,21 +16,108 @@ class SugerenciasScreen extends StatefulWidget {
 
 class _SugerenciasScreenState extends State<SugerenciasScreen> {
   TextEditingController _feedbackController = TextEditingController();
+  List<Carrera> listacarreras = [];
+  late Sugerencias sugerencias;
+  TextEditingController _carreracon = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     _feedbackController = TextEditingController();
+
+    sugerencias = Sugerencias(
+      id: 0,
+      descripcion: '',
+      fecha: DateTime.now(),
+      persona:
+          Persona(), // Asegúrate de que esta sea una inicialización adecuada para tu clase Persona
+      carrera:
+          Carrera(), // Asegúrate de que esta sea una inicialización adecuada para tu clase Carrera
+    );
+
+    // Determine user role and call appropriate function
+    String rol = 'Estudiante'; // Set role based on user
+    if (rol == 'Docente') {
+      getcarreras();
+    } else if (rol == 'Estudiante') {
+      getcarrerascedula();
+    }
   }
 
   @override
   void dispose() {
     _feedbackController.dispose();
+    _carreracon.dispose();
     super.dispose();
   }
 
   void _submitFeedback() {
     crearsugerencia();
+  }
+
+/*Busca las carreras asociadas a una cédula de un solicitante de préstamo 
+  si esta tiene la función de docente  en la base de datos, se muestra todos las carreras que existen,
+  pero si en la busqueda la cedula pertenece a un estudiante solo se mostrara la carrera con la que esta asociada.
+  Los datos de las carreras, se almacenan en una lista llamada listacarreras.*/
+  Future<void> getcarrerascedula() async {
+    try {
+      List<Carrera> carreras = [];
+      var url = Uri.parse('$baseUrl/carrera/carreraest/${personalog.cedula}');
+      final response = await http.get(url, headers: headers);
+
+      if (response.statusCode == 200) {
+        Map<String, dynamic> jsonResponse = json.decode(response.body);
+        print(response.body);
+        Carrera carrera = Carrera.fromJson(jsonResponse);
+
+        carreras.add(carrera);
+        setState(() {
+          listacarreras = carreras;
+          _carreracon.text = carrera.nombre;
+          sugerencias.carrera = carrera;
+        });
+
+        setState(() {
+          listacarreras = carreras;
+        });
+      } else {
+        print(
+            'Error en la solicitud GET lista carreras: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error get carreras $error');
+    }
+  }
+
+  /*Busca y obtiene una lista de todas las carreras disponibles en la base de datos. 
+  Los datos de las carreras se almacenan en la lista listacarreras */
+  Future<void> getcarreras() async {
+    try {
+      List<Carrera> carreras = [];
+      var url = Uri.parse('$baseUrl/carrera/listar');
+      final response = await http.get(url, headers: headers);
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonResponse = json.decode(response.body);
+        print(response.body);
+        for (dynamic Json in jsonResponse) {
+          Carrera carrera = Carrera.fromJson(Json);
+
+          carreras.add(carrera);
+          setState(() {
+            listacarreras = carreras;
+          });
+        }
+        setState(() {
+          listacarreras = carreras;
+        });
+      } else {
+        print(
+            'Error en la solicitud GET lista carreras: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error get carreras $error');
+    }
   }
 
   /*Crear una nueva sugerencia y enviarla a la base de datos mediante una solicitud POST. 
@@ -106,6 +195,25 @@ class _SugerenciasScreenState extends State<SugerenciasScreen> {
                 border: OutlineInputBorder(),
               ),
             ),
+            const SizedBox(height: 16.0),
+            // DropdownButton added here
+            listacarreras.isNotEmpty
+                ? DropdownButton<Carrera>(
+                    value: sugerencias.carrera,
+                    onChanged: (Carrera? newValue) {
+                      setState(() {
+                        sugerencias.carrera = newValue!;
+                      });
+                    },
+                    items: listacarreras
+                        .map<DropdownMenuItem<Carrera>>((Carrera carrera) {
+                      return DropdownMenuItem<Carrera>(
+                        value: carrera,
+                        child: Text(carrera.nombre),
+                      );
+                    }).toList(),
+                  )
+                : Container(), // Display empty container if listacarreras is empty
             const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: _submitFeedback,
--- lib/models/sugerencia.dart
+++ lib/models/sugerencia.dart
@@ -1,24 +1,28 @@
 import 'dart:convert';
 
 import 'package:proyectoistateca/models/persona.dart';
+import 'package:proyectoistateca/models/carrera.dart';
 
 class Sugerencias {
   final int id;
   final String descripcion;
   final DateTime fecha;
   final Persona persona;
+  Carrera carrera;
 
   Sugerencias(
       {required this.id,
       required this.descripcion,
       required this.fecha,
-      required this.persona});
+      required this.persona,
+      required this.carrera});
 
   factory Sugerencias.fromJson(Map<String, dynamic> json) {
     return Sugerencias(
         id: json['id'] as int? ?? 0,
         descripcion: utf8.decode(json['descripcion'].toString().codeUnits),
         fecha: DateTime.parse(json['fecha']),
-        persona: Persona.fromJson(json['persona']));
+        persona: Persona.fromJson(json['persona']),
+        carrera: Carrera.fromJson(json['carrera'] ?? {}));
   }
 }
